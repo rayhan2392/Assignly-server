d43f870bb5241f285ea4096458b838fa201529f2
rkshawn <rkshawn975@gmail.com>
2024-05-15 01:37:19 +0600

    vercel json file for deployin in vercel

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ app.put('/assignments/:id',async(req,res)=>{
 })
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error

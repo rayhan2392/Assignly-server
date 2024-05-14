ad2bddfe517713d1c770e923cf4f3f5d8593c262
rkshawn <rkshawn975@gmail.com>
2024-05-14 13:55:37 +0600

    api created for get a single assignment data

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express')
 require('dotenv').config()
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 
 const port = process.env.PORT || 5000
 
 //middle wares
 app.use(cors());
-app.use(express());
+app.use(express.json());
 
 app.get('/',(req,res)=>{
     res.send('assignly server is running')
@@ -28,14 +28,57 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    const assignmentDataCollection =client.db('assignlyDB').collection.apply('assignmentDB')
+    const assignmentDataCollection =client.db('assignlyDB').collection('assignmentDB')
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 //post assignment data created by user 
 app.post('/assignments', async(req,res)=>{
    const newAssignment =req.body;
    console.log(newAssignment)
+  const result = await assignmentDataCollection.insertOne(newAssignment);
+  res.send(result)
    
+})
+
+//get all assignment data
+app.get('/assignments',async(req,res)=>{
+  const cursor = assignmentDataCollection.find();
+  const result =await cursor.toArray();
+  res.send(result);
+})
+
+//delete assignment data
+app.delete('/assignments/:id',async(req,res)=>{
+  const id =req.params.id;
+  const query ={_id:new ObjectId(id)}
+  const result = await assignmentDataCollection.deleteOne(query)
+  res.send(result);
+})
+
+//get single travel data
+app.get('/assignments/:id',async(req,res)=>{
+  const id = req.params.id;
+  const query ={_id:new ObjectId(id)}
+  const result =await assignmentDataCollection.findOne(query);
+  res.send(result);
+})
+
+//update an assignment data
+app.put('/assignments/:id',async(req,res)=>{
+  const id = req.params.id;
+  const filter = {_id:new ObjectId(id)}
+  const currentAssignment = req.body;
+  const options = {upsert:true}
+  const updatedAssignment ={
+    $set:{
+      title:currentAssignment.title,
+      img:currentAssignment.img,
+      marks:currentAssignment.marks,
+      description:currentAssignment.description
+    }
+  } 
+  const result = await assignmentDataCollection.updateOne(filter,updatedAssignment,options);
+  res.send(result);
 })
 
     // Send a ping to confirm a successful connection
